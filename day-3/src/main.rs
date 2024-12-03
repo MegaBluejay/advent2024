9274fbdcca7d241443a9192366e623d6f754f6ac
MegaBluejay <dmoiseev2011@gmail.com>
2024-12-03 11:35:06 +0300

    faster day 3

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,28 +1,28 @@
 use std::{env, fs::File};
 
 use anyhow::{Context, Result};
-use atoi::atoi;
 use memmap2::Mmap;
 use regex::bytes::Regex;
+use util::atoi_with_rest;
 
 fn main() -> Result<()> {
     let filename = env::args().nth(1).context("no input file arg")?;
     let file = File::open(filename)?;
     let input = unsafe { Mmap::map(&file)? };
 
-    let re = Regex::new(r"mul\((\d+),(\d+)\)|do\(\)|don't\(\)")?;
+    let re = Regex::new(r"mul\(\d+,\d+\)|do\(\)|don't\(\)")?;
 
     let mut ans1: u64 = 0;
     let mut ans2: u64 = 0;
     let mut enabled = true;
 
-    for m in re.captures_iter(&input) {
-        match &m[0] {
+    for m in re.find_iter(&input) {
+        match m.as_bytes() {
             b"do()" => enabled = true,
             b"don't()" => enabled = false,
-            _ => {
-                let a: u64 = atoi(&m[1]).context("invalid int")?;
-                let b: u64 = atoi(&m[2]).context("invalid int")?;
+            s => {
+                let (a, s) = atoi_with_rest::<u64>(&s[4..])?;
+                let (b, _) = atoi_with_rest::<u64>(&s[1..])?;
                 let res = a * b;
                 ans1 += res;
                 if enabled {
