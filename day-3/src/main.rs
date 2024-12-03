1e214808697d40ad2c74dab1200cab30dcd46f6a
MegaBluejay <dmoiseev2011@gmail.com>
2024-12-03 12:17:36 +0300

    single pass

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,56 +1,48 @@
 use std::{env, fs::File};
 
 use anyhow::{anyhow, Context, Result};
-use itertools::Itertools;
 use memmap2::Mmap;
 use util::atoi_with_rest;
-
-fn part1(s: &[u8]) -> u64 {
-    let mut ans = 0;
-
-    for i in s.windows(4).positions(|q| q == b"mul(") {
-        if let Ok((a, b)) = atoi_with_rest::<u64>(&s[i + 4..]).and_then(|(a, rest)| {
-            let (b, rest) = atoi_with_rest::<u64>(&rest[1..])?;
-            if rest.is_empty() || rest[0] != ')' as u8 {
-                return Err(anyhow!("no )"));
-            }
-            Ok((a, b))
-        }) {
-            ans += a * b;
-        }
-    }
-
-    ans
-}
-
-fn part2(mut s: &[u8]) -> u64 {
-    let mut ans = 0;
-
-    loop {
-        let end = s
-            .windows(7)
-            .position(|q| q == b"don't()")
-            .unwrap_or(s.len());
-        ans += part1(&s[..end]);
-        s = &s[end + 7..];
-        let start = s.windows(4).position(|q| q == b"do()");
-        if let Some(start) = start {
-            s = &s[start + 4..];
-        } else {
-            break;
-        }
-    }
-
-    ans
-}
 
 fn main() -> Result<()> {
     let filename = env::args().nth(1).context("no input file arg")?;
     let file = File::open(filename)?;
-    let input = unsafe { Mmap::map(&file)? };
+    let input_all = unsafe { Mmap::map(&file)? };
 
-    let ans1 = part1(&input);
-    let ans2 = part2(&input);
+    let mut input = &input_all[..];
+    let mut enabled = true;
+    let mut ans1 = 0;
+    let mut ans2 = 0;
+
+    while input.len() >= 4 {
+        if &input[..4] == b"mul(" {
+            if let Ok((a, b, rest)) = atoi_with_rest::<u64>(&input[4..]).and_then(|(a, rest)| {
+                let (b, rest) = atoi_with_rest::<u64>(&rest[1..])?;
+                if rest.is_empty() || rest[0] != ')' as u8 {
+                    Err(anyhow!("no )"))
+                } else {
+                    Ok((a, b, rest))
+                }
+            }) {
+                let res = a * b;
+                ans1 += res;
+                if enabled {
+                    ans2 += res;
+                }
+                input = &rest[1..];
+            } else {
+                input = &input[4..];
+            }
+        } else if &input[..4] == b"do()" {
+            enabled = true;
+            input = &input[4..];
+        } else if input.len() >= 7 && &input[..7] == b"don't()" {
+            enabled = false;
+            input = &input[7..];
+        } else {
+            input = &input[1..];
+        }
+    }
 
     println!("{ans1}");
     println!("{ans2}");
