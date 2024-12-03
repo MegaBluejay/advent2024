863450bba7faf99460343a0b5a2da1296876ed34
MegaBluejay <dmoiseev2011@gmail.com>
2024-12-03 12:06:54 +0300

    even faster regex-less

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,36 +1,56 @@
 use std::{env, fs::File};
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
+use itertools::Itertools;
 use memmap2::Mmap;
-use regex::bytes::Regex;
 use util::atoi_with_rest;
+
+fn part1(s: &[u8]) -> u64 {
+    let mut ans = 0;
+
+    for i in s.windows(4).positions(|q| q == b"mul(") {
+        if let Ok((a, b)) = atoi_with_rest::<u64>(&s[i + 4..]).and_then(|(a, rest)| {
+            let (b, rest) = atoi_with_rest::<u64>(&rest[1..])?;
+            if rest.is_empty() || rest[0] != ')' as u8 {
+                return Err(anyhow!("no )"));
+            }
+            Ok((a, b))
+        }) {
+            ans += a * b;
+        }
+    }
+
+    ans
+}
+
+fn part2(mut s: &[u8]) -> u64 {
+    let mut ans = 0;
+
+    loop {
+        let end = s
+            .windows(7)
+            .position(|q| q == b"don't()")
+            .unwrap_or(s.len());
+        ans += part1(&s[..end]);
+        s = &s[end + 7..];
+        let start = s.windows(4).position(|q| q == b"do()");
+        if let Some(start) = start {
+            s = &s[start + 4..];
+        } else {
+            break;
+        }
+    }
+
+    ans
+}
 
 fn main() -> Result<()> {
     let filename = env::args().nth(1).context("no input file arg")?;
     let file = File::open(filename)?;
     let input = unsafe { Mmap::map(&file)? };
 
-    let re = Regex::new(r"mul\(\d{1,3},\d+{1,3}\)|do\(\)|don't\(\)")?;
-
-    let mut ans1: u64 = 0;
-    let mut ans2: u64 = 0;
-    let mut enabled = true;
-
-    for m in re.find_iter(&input) {
-        match m.as_bytes() {
-            b"do()" => enabled = true,
-            b"don't()" => enabled = false,
-            s => {
-                let (a, s) = atoi_with_rest::<u64>(&s[4..])?;
-                let (b, _) = atoi_with_rest::<u64>(&s[1..])?;
-                let res = a * b;
-                ans1 += res;
-                if enabled {
-                    ans2 += res;
-                }
-            }
-        }
-    }
+    let ans1 = part1(&input);
+    let ans2 = part2(&input);
 
     println!("{ans1}");
     println!("{ans2}");
